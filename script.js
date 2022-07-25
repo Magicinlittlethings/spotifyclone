25f8bc8476c8cdb8ecd606eb1c38cda2dd32a7c6
lawal <ladeholar@gmail.com>
2022-07-25 13:59:29 -0700

    newupdates

--- script.js
+++ script.js
@@ -14,43 +14,237 @@ tl.to('.availablepositions',  {
 
 const tl2 = gsap.timeline({
     scrollTrigger:{
-        trigger: ".second",
         scrub:true,
-        pin: '.first', 
-        pinSpacing: false,
         
       }
       
     }
-)
-tl2.from('.whoweare',{
-    height:80,
-    scrollTrigger:{
-        trigger: ".whoweare",
-        scrub:true,
-        start: "top 70%", 
-        
-        pin: '.wherewebelong', 
-        pinSpacing: false,
-        
-      },
+)/*
+tl2.to('.whoweare',{
+    y:-1100,
     
-}
+        scrollTrigger:{
+            trigger: ".whoweare",
+            start:"top 60%",
+            pin: '.second',
+            scrub:true,
+            pinSpacing: false,
+            
+            
+          }
+    }
+
+
 ) 
-tl2.from('.whoweare',{
-    height:80,
-    scrollTrigger:{
-        trigger: ".whoweare",
-        scrub:true,
-        start: "top", 
-        
-        pin:'whoweare',
-        pinSpacing: false,
-        
-      },
+
+tl2.to('.wherewebelong',{
+    y:-1400,
     
+        scrollTrigger:{
+            
+            trigger: ".whoweare",
+            start:"top -2%",
+            pin: true,
+            
+            scrub:true,
+            pinSpacing: false,
+            
+          }
+    }
+)
+
+tl2.to('.howweact',{
+
+    
+    y:-2000,
+    
+        scrollTrigger:{
+            
+            trigger: ".howweact",
+            start:"top -6%",
+            scrub:true,
+            pinSpacing: false,
+            
+          }
 }
 )
+
+*/
+tl2.to('.whoweare',{
+   
+    
+        
+    }
+
+
+) 
+
+tl2.to('.wherewebelong',{
+   
+    
+        scrollTrigger:{
+            
+            trigger: ".wherewebelong",
+            start:"top 100%",
+            end:"bottom 24%",
+            pin: ".whoweare",
+            scrub:true,
+            pinSpacing: false,
+            
+          }
+    }
+)
+
+
+tl2.to('.howweact',{
+
+    
+    
+    
+        scrollTrigger:{
+            
+            trigger: ".howweact",
+            start:"top 112%",
+            end:"top 24%",
+            
+            pin:".wherewebelong",
+            scrub:true,
+            pinSpacing: false,
+            
+          }
+}
+)
+const tl3 = gsap.timeline();
+
+tl3.to('.bodyy',  {
+  backgroundColor:'#2158a0',
+  duration:1,
+  scrollTrigger:{
+     trigger:".third",   
+     scrub:true,
+     start:"top 35%"   ,
+     end:"top -60%",
+     toggleActions:"  play none reverse reset",
+    
+  }
+  
+    
+    
+   
+})
+
+tl3.to('.bodyy',  {
+  backgroundColor:'#ffff',
+  duration:1,
+  scrollTrigger:{
+     trigger:".here",   
+     scrub:true,
+     start:"top 35%"   ,
+     end:"top ",
+     toggleActions:"  play reset restart reset",
+    
+  }
+  
+    
+  
+  
+   
+})
+tl3.to('.bodyy',  {
+  backgroundColor:'#ffccd5',
+  duration:1,
+  scrollTrigger:{
+     trigger:".fifth",   
+     scrub:true,
+     start:"top 35%"   ,
+     end:"top -60%",
+     toggleActions:"  play none reverse reset",
+    
+  }
+ 
+    
+    
+   
+})
+
+tl3.to('.bodyy',  {
+  backgroundColor:'#ffff',
+  duration:1,
+  scrollTrigger:{
+     trigger:".sixth",   
+     scrub:true,
+     start:"top 35%"   ,
+     end:"top ",
+     toggleActions:"  play reset restart reset",
+    
+  }
+  
+    
+  
+  
+   
+})
+
+tl3.to('.opp',  {
+  backgroundColor:'#caf0f8',
+  scrollTrigger:{
+     trigger:".third",
+     scrub:true,  
+     start:"top 35%%"   ,
+     end:"top -60%",
+     toggleActions:"  play reset play reset",
+    
+  }
+
+    
+    
+   
+})
+tl3.to('.opp',  {
+  backgroundColor:'#caf0f8',
+  scrollTrigger:{
+     trigger:".here",
+     scrub:true,  
+     start:"top 35%"   ,
+     end:"top",
+     toggleActions:"  play reset play reset",
+    
+  }
+
+    
+    
+   
+})
+tl3.to('.feat',  {
+  color:'#ffff',
+  scrollTrigger:{
+     trigger:".third",   
+     start:"top 35%"   ,
+     end:"top -50%",
+    
+  }
+
+    
+    
+   
+})
+tl3.to('.feat',  {
+  color:'#000000',
+  scrollTrigger:{
+     trigger:".here",
+     scrub:true,  
+     start:"top 35%"   ,
+     end:"top",
+     toggleActions:"  play reset play reset",
+    
+  }
+
+    
+    
+   
+})
+
+
 
 
 /*
@@ -60,7 +254,24 @@ tl.addEventListener('mouseover', () =>{
 })*/
 
 
-  
+   var swiper = new Swiper(".swiper", {
+        slidesPerView: "auto",
+        centeredSlides: false,
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+      var swiper = new Swiper(".swiperr", {
+        slidesPerView: "auto",
+        centeredSlides: false,
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
   
   
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'darkgreen1':'#386641',
+        'darkgreen2':'#004b23',
+
+      }
+    },
   },
   plugins: [],
 }
